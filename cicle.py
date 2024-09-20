c242ba719c365cb00b223fdb82fc72eddecb98d4
Artyom <kostyuchenko.work@gmail.com>
2024-09-20 10:51:48 +0300

    first commit

---
+++ cicle.py
@@ -0,0 +1,20 @@
+lst = [1,2,3,4,5,6,7,8]
+
+# lst = ['строка',True, [1,2,3], 205, .5]
+# for i in range(len(lst)):
+
+# for val in lst:
+#     print(f"{val=}")
+#     print('S[окр] =', 3.14*val**2)
+
+
+print(range(20))
+print(range(5,10))
+print(range(2, 19, 2))
+
+# for ind in range(2,19,2):
+#     print(f'{ind=}')
+
+lst = [1,2,3,4,5,6,7,8]
+for i in range(len(lst),):
+    print(f'{i=} | {lst[i]}')
